3ef2f217195c12c0591315a92bd235309f94ba76
fengqi <lyf362345@gmail.com>
2024-07-13 07:27:41 +0000

    使用pinia状态判断

--- src/components/top.vue
+++ src/components/top.vue
@@ -2,32 +2,30 @@
   <v-container>
       <div class="d-flex">
         <v-btn class="ma-2 pa-2" @click="$router.push('/')">首页</v-btn>
-        <v-btn v-show="!isLoggedIn" class="ma-2 pa-2" @click="$router.push('/user/register')">注册</v-btn>
-        <v-btn v-show="!isLoggedIn" class="ma-2 pa-2" @click="$router.push('/user/login')">登录</v-btn>
-        <v-btn v-show="isLoggedIn" class="ma-2 pa-2" @click="$router.push('/user/logout')">退出</v-btn>
+        <v-btn v-show="!store.online" class="ma-2 pa-2" @click="$router.push('/user/register')">注册</v-btn>
+        <v-btn v-show="!store.online" class="ma-2 pa-2" @click="$router.push('/user/login')">登录</v-btn>
+        <v-btn v-show="store.online" class="ma-2 pa-2" @click="$router.push('/user/logout')">退出</v-btn>
       </div>
   </v-container>
 </template>
 
 <script>
 
+import { useAppStore } from "@/stores/app";
+
 export default {
+  setup() {
+    const store = useAppStore();
+    return { store }
+  },
+
   data: () => ({
-    isLoggedIn: false,
   }),
 
   mounted() {
-    if (localStorage.getItem("jwt_token")) {
-      this.isLoggedIn = true
-    } else {
-      this.isLoggedIn = false
-    }
   },
 
   methods: {
-    showLoginModal() {
-      // Implement your login modal logic here
-    },
   },
 };
 </script>
