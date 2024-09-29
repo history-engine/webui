55e93764dd090cfd2e5f1f78d599417cef4d360a
fengqi <lyf362345@gmail.com>
2024-09-29 14:53:56 +0800

    初步完成存储设置表单

--- src/pages/setting/storage.vue
+++ src/pages/setting/storage.vue
@@ -1,6 +1,47 @@
 <template>
   <v-container fluid>
-    storage
+    <v-form validate-on="submit" rounded="lg" @submit.prevent>
+      <div class="text-subtitle-1 text-medium-emphasis">最大版本数</div>
+      <v-number-input
+        class="mb-4"
+        hint="超过设置的版本数时，将自动删除最早的版本。"
+        :rules="[rules.required]"
+        required
+        :min="1"
+        variant="outlined"
+      ></v-number-input>
+
+      <div class="text-subtitle-1 text-medium-emphasis">最小保存间隔（秒）</div>
+      <v-text-field
+        class="mb-4"
+        hint="此间隔时间内，重复访问一个网页，不再保存。"
+        :rules="[rules.required]"
+        required
+        type="number"
+        variant="outlined"
+      ></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis">最小HTML文件大小</div>
+      <v-text-field
+        class="mb-4"
+        hint="小于此大小的HTML文件不会被保存，1MB=1024*1024=1,048,576字节"
+        :rules="[rules.required]"
+        required
+        type="number"
+        variant="outlined"
+      ></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis">最大HTML文件大小</div>
+      <v-text-field
+        hint="大于此大小的HTML文件不会被保存，1MB=1024*1024=1,048,576字节"
+        :rules="[rules.required]"
+        required
+        type="number"
+        variant="outlined"
+      ></v-text-field>
+
+      <v-btn variant="outlined">保存</v-btn>
+    </v-form>
   </v-container>
 </template>
 
