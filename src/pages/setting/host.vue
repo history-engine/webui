25810b4574174731b1911e4ea1001554dd7dcdaa
fengqi <lyf362345@gmail.com>
2024-10-01 13:53:18 +0800

    完成host的修改、删除

--- src/pages/setting/host.vue
+++ src/pages/setting/host.vue
@@ -1,4 +1,77 @@
 <template>
+  <div class="text-center">
+    <v-dialog v-model="editDialog" max-width="600" persistent>
+      <v-card prepend-icon="mdi-account" title="修改host">
+        <v-card-text>
+          <v-row dense>
+            <v-col cols="12" sm="12">
+              <v-text-field
+                label="Id*"
+                v-model="id"
+                readonly
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="12">
+              <v-select
+                :items="types"
+                label="类型"
+                v-model="type"
+                :hint="`${type.state}, ${type.abbr}`"
+                item-title="state"
+                item-value="abbr"
+                return-object
+                required
+              ></v-select>
+            </v-col>
+
+            <v-col cols="12" sm="12">
+              <v-text-field
+                hint="支持完整域名a.b.cn、通配符*.b.cn、正则regexp:.*(\.|)(a|b|c|d)\.cn"
+                label="host"
+                v-model="host"
+                required
+              ></v-text-field>
+            </v-col>
+
+          </v-row>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            text="取消"
+            variant="plain"
+            @click="editDialog = false"
+          ></v-btn>
+
+          <v-btn
+            color="primary"
+            text="提交"
+            variant="tonal"
+            @click="submitEdit"
+          ></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="deleteDialog" max-width="400">
+      <v-card
+        prepend-icon="mdi-delete-alert"
+        title="确定删除吗？"
+      >
+        <template v-slot:actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="deleteDialog = false">取消</v-btn>
+          <v-btn color="primary" @click="submitDelete">确认</v-btn>
+        </template>
+      </v-card>
+    </v-dialog>
+  </div>
+
   <v-container fluid>
     <v-row align="center" justify="end">
       <v-col cols="10">
@@ -34,8 +107,8 @@
           <td>{{ item.type == 1 ? '包括' : '忽略' }}</td>
           <td>{{ item.created_at }}</td>
           <td>
-<!--            <v-btn class="mr-2">删除</v-btn>-->
-<!--            <v-btn>修改</v-btn>-->
+            <v-btn @click="openDeleteDialog(item.id)" class="mr-2">删除</v-btn>
+            <v-btn @click="openEditDialog(item.id, item.type, item.host)">修改</v-btn>
           </td>
         </tr>
       </template>
@@ -70,9 +143,75 @@ export default {
     ],
     keyword: '',
     loading: true,
+    types: [
+      { state: '包括', abbr: 1 },
+      { state: '忽略', abbr: 2 },
+    ],
+    id: 0,
+    type: {},
+    host: '',
+    deleteDialog: false,
+    editDialog: false,
   }),
 
   methods: {
+    openDeleteDialog(id) {
+      this.id = id
+      this.deleteDialog = true;
+    },
+
+    openEditDialog(id, type, host) {
+      this.id = id
+      this.type = { state: type == 1 ?  '包括' : '忽略', abbr: type }
+      this.host = host
+      this.editDialog = true;
+    },
+
+    submitDelete() {
+      http({
+        method: "delete",
+        url: "/setting/host",
+        params: {
+          id: this.id,
+        }
+      }).then(resp => {
+        if (resp.code == 0) {
+          this.search()
+        } else {
+          this.alert(resp.message)
+        }
+      }).catch(err => {
+        this.alert('操作失败：' + err)
+      });
+
+      this.id = 0
+      this.host = ''
+      this.deleteDialog = false;
+    },
+
+    submitEdit() {
+      http({
+        method: "post",
+        url: "/setting/host",
+        data: {
+          id: this.id,
+          type: this.type.abbr,
+          host: this.host,
+        }
+      }).then(resp => {
+        if (resp.code == 0) {
+          this.search()
+          this.id = 0
+          this.host = ''
+          this.editDialog = false;
+        } else {
+          this.alert(resp.message)
+        }
+      }).catch(err => {
+        this.alert('操作失败：' + err)
+      });
+    },
+
     search() {
       this.loadItems({
         page:this.page,
