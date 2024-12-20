dd1be98861305240ed11299ccb0ef4efdb27f2b4
fengqi <lyf362345@gmail.com>
2024-12-20 18:34:36 +0800

    后台导航

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -7,30 +7,17 @@
         <v-row>
           <v-col cols="2">
             <v-sheet rounded="lg">
-              <v-list rounded="lg">
-                <v-list-item
-                  v-for="n in 5"
-                  :key="n"
-                  :title="`List Item ${n}`"
-                  link
-                ></v-list-item>
-
-                <v-divider class="my-2"></v-divider>
-
-                <v-list-item
-                  color="grey-lighten-4"
-                  title="Refresh"
-                  link
-                ></v-list-item>
+              <v-list rounded="lg" class="pa-2">
+                <v-list-item title="用户管理" link></v-list-item>
+                <v-list-item title="Icon管理" link></v-list-item>
+                <v-list-item title="杂项设置" link></v-list-item>
+                <v-list-item title="文件管理" link></v-list-item>
               </v-list>
             </v-sheet>
           </v-col>
 
           <v-col>
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
+            <v-sheet min-height="70vh" rounded="lg" class="pa-2">
               <router-view />
             </v-sheet>
           </v-col>
